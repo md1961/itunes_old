66969cf9001b594024589e26c5f4995cbaec5b9f
Naoyuki Kumagai <kumagai@namaka>
2016-08-31 13:14:17 +0900

    Move helper str_time_in_min_and_sec() to ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 module ApplicationHelper
 
+  def str_time_in_min_and_sec(millisecond)
+    second = (millisecond / 1000.0).round
+    '%02d:%02d' % [second / 60, second % 60]
+  end
+
   def error_messages_for(model_instance)
     return if model_instance.errors.empty?
     render 'error_messages_for', model_instance: model_instance
--- app/helpers/tracks_helper.rb
+++ app/helpers/tracks_helper.rb
@@ -1,7 +1,2 @@
 module TracksHelper
-
-  def str_time_in_min_and_sec(millisecond)
-    second = (millisecond / 1000.0).round
-    '%02d:%02d' % [second / 60, second % 60]
-  end
 end
