8db61df09cc588b57777180c4de4a088e9d6751a
Naoyuki Kumagai <kumagai@namaka>
2016-08-31 13:23:48 +0900

    Alter helper str_time_in_min_and_sec() to receive arg in second

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,6 @@
 module ApplicationHelper
 
-  def str_time_in_min_and_sec(millisecond)
-    second = (millisecond / 1000.0).round
+  def str_time_in_min_and_sec(second)
     '%02d:%02d' % [second / 60, second % 60]
   end
 
