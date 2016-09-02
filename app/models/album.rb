3f7e7215b9708f28510a2a887041e4043c0ba575
Naoyuki Kumagai <kumagai@namaka>
2016-09-02 10:24:19 +0900

    Add Album#compilation? and modify .all for that

--- app/models/album.rb
+++ app/models/album.rb
@@ -7,17 +7,28 @@ class Album
 
     @@initial_id = 1
 
-    def initialize(name, artist_name)
+    def initialize(name, artist_name, is_compilation=false)
       @id = @@initial_id
       @name = name
       @artist = Artist.find_by(name: artist_name)
+      @is_compilation = is_compilation
 
       @@initial_id += 1
     end
     private_class_method :new
 
   def self.all
-    @all_instances ||= Track.pluck(:album, :artist).uniq.map { |name, artist_name| new(name, artist_name) }
+    @all_instances ||= make_all_instances
+  end
+
+    def self.make_all_instances
+        Track.where(compilation: false).pluck(:album, :artist).uniq.map { |name, artist_name| new(name, artist_name) } \
+      + Track.where(compilation: true ).pluck(:album         ).uniq.map { |name             | new(name, nil, true  ) }
+    end
+    private_class_method :make_all_instances
+
+  def compilation?
+    @is_compilation
   end
 
   def tracks
